7c5bb7d52a83d53a7f1917bf32b57427fe6b9664
Gunjan Patidar <gunjan2307@gmail.com>
2024-07-15 19:00:06 +0530

    fix status model to make the responses more usable

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -33,7 +33,7 @@ func AuthHandler(c *gin.Context) {
 	// generate request id and send response
 	request_id := uuid.Must(uuid.NewRandom())
 	c.JSON(http.StatusOK, models.AuthResponse{
-		Status:    models.StatusSuccess(""),
+		Status:    models.StatusSuccess(),
 		RequestID: request_id,
 	})
 }
