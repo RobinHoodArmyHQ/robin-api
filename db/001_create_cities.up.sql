6de72fd3b5b5e40e6553a27f1bd8a1926da00b90
Gunjan Patidar <gunjan2307@gmail.com>
2024-07-27 09:31:22 +0530

    fix table charsets

--- db/001_create_cities.up.sql
+++ db/001_create_cities.up.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS `cities` (
     `country_id` TINYINT UNSIGNED NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE `idx_city_id` (`city_id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
--- db/002_create_countries.up.sql
+++ db/002_create_countries.up.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS `countries` (
     `country_id` VARCHAR(15) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL ,
     `name` VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL ,
     PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
